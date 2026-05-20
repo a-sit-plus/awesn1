3d26483e9f69fdf73716c1008f0e89a53788c66c
Bernd Prünster <bernd.pruenster@a-sit.at>
2026-05-20 17:45:04 +0200

    Update crypto/src/commonMain/kotlin/at/asitplus/awesn1/crypto/pki/Pkcs10CsrAttribute.kt
    Co-authored-by: Jakob Heher <jakob.heher@iaik.tugraz.at>

--- crypto/src/commonMain/kotlin/at/asitplus/awesn1/crypto/pki/Pkcs10CsrAttribute.kt
+++ crypto/src/commonMain/kotlin/at/asitplus/awesn1/crypto/pki/Pkcs10CsrAttribute.kt
@@ -31,7 +31,7 @@ data class Pkcs10CsrAttribute(
     companion object {
         val EXTENSION_REQUEST_OID = ObjectIdentifier("1.2.840.113549.1.9.14")
 
-        fun wrapExtensions(extensions: List<X509CertificateExtension>): Pkcs10CsrAttribute {
+        fun ExtensionRequest(extensions: List<X509CertificateExtension>): Pkcs10CsrAttribute {
             require(extensions.isNotEmpty()) { "At least one extension is required" }
             return Pkcs10CsrAttribute(
                 EXTENSION_REQUEST_OID,
