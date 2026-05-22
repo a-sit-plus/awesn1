ed927edb4c55b5c7cd7bfbc691389d482ca28b7d
Bernd Prünster <bernd.pruenster@a-sit.at>
2026-05-22 11:19:15 +0200

    rename serializers

--- core/src/commonMain/kotlin/at/asitplus/awesn1/SerializationDescriptorNames.kt
+++ core/src/commonMain/kotlin/at/asitplus/awesn1/SerializationDescriptorNames.kt
@@ -4,26 +4,26 @@
 package at.asitplus.awesn1
 
 @InternalAwesn1Api
-const val ASN1_DESCRIPTOR_OBJECT_IDENTIFIER: String = "OID"
+const val ASN1_DESCRIPTOR_OBJECT_IDENTIFIER: String = "at.asitplus.awesn1.ObjectIdentifier"
 
 @InternalAwesn1Api
-const val ASN1_DESCRIPTOR_INTEGER: String = "ASN.1 Integer"
+const val ASN1_DESCRIPTOR_INTEGER: String = "at.asitplus.awesn1.Asn1Integer"
 
 @InternalAwesn1Api
-const val ASN1_DESCRIPTOR_REAL: String = "Asn1Real"
+const val ASN1_DESCRIPTOR_REAL: String = "at.asitplus.awesn1.Asn1Real"
 
 @InternalAwesn1Api
-const val ASN1_DESCRIPTOR_STRING: String = "Asn1String"
+const val ASN1_DESCRIPTOR_STRING: String = "at.asitplus.awesn1.Asn1String"
 
 @InternalAwesn1Api
-const val ASN1_DESCRIPTOR_TIME: String = "Asn1Time"
+const val ASN1_DESCRIPTOR_TIME: String = "at.asitplus.awesn1.Asn1Time"
 
 @InternalAwesn1Api
-const val ASN1_DESCRIPTOR_BIT_STRING: String = "Asn1BitString"
+const val ASN1_DESCRIPTOR_BIT_STRING: String = "at.asitplus.awesn1.Asn1BitString"
 
 @InternalAwesn1Api
-const val ASN1_DESCRIPTOR_ELEMENT_TREE: String = "Asn1ElementDerEncodedSerializer"
+const val ASN1_DESCRIPTOR_ELEMENT_TREE: String = "at.asitplus.awesn1.Asn1ElementDerEncodedSerializer"
 
 @InternalAwesn1Api
-const val ASN1_DESCRIPTOR_OPAQUE: String = "Asn1DerSerializer"
+const val ASN1_DESCRIPTOR_OPAQUE: String = "at.asitplus.awesn1.Asn1Serializable"
 
