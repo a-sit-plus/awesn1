9108d92acf6f6240dea8fa84f5959f774a0a49a5
Jakob Heher <jakob.heher@iaik.tugraz.at>
2026-07-22 10:57:39 +0200

    and do so everywhere

--- crypto/src/appleMain/kotlin/at/asitplus/awesn1/crypto/pki/X509GeneralNames.apple.kt
+++ crypto/src/appleMain/kotlin/at/asitplus/awesn1/crypto/pki/X509GeneralNames.apple.kt
@@ -1,3 +1,4 @@
+@file:OptIn(ExperimentalObjCName::class)
 // SPDX-FileCopyrightText: Copyright (c) A-SIT Plus GmbH
 // SPDX-License-Identifier: Apache-2.0
 
@@ -5,27 +6,32 @@ package at.asitplus.awesn1.crypto.pki
 
 import at.asitplus.awesn1.Asn1Exception
 import at.asitplus.awesn1.Asn1StructuralException
+import kotlin.experimental.ExperimentalObjCName
 
 /**
  * @see X509GeneralName.Rfc822.value
  */
 @Throws(Asn1Exception::class)
-fun X509GeneralName.Rfc822.value(): String = value
+@ObjCName("value")
+fun __apple_workaround_X509GeneralName_Rfc822_value(it: X509GeneralName.Rfc822) = it.value
 
 /**
  * @see X509GeneralName.Dns.value
  */
 @Throws(Asn1Exception::class)
-fun X509GeneralName.Dns.value(): String = value
+@ObjCName("value")
+fun __apple_workaround_X509GeneralName_Dns_value(it: X509GeneralName.Dns) = it.value
 
 /**
  * @see X509GeneralName.UniformResourceIdentifier.value
  */
 @Throws(Asn1Exception::class)
-fun X509GeneralName.UniformResourceIdentifier.value(): String = value
+@ObjCName("value")
+fun __apple_workaround_X509GeneralName_URI_value(it: X509GeneralName.UniformResourceIdentifier) = it.value
 
 /**
  * @see X509GeneralName.IpAddress.value
  */
 @Throws(Asn1StructuralException::class)
-fun X509GeneralName.IpAddress.value(): ByteArray = value
+@ObjCName("value")
+fun __apple_workaround_X509GeneralName_IpAddress_value(it: X509GeneralName.IpAddress) = it.value
--- crypto/src/appleMain/kotlin/at/asitplus/awesn1/crypto/pki/X509TbsCertificate.apple.kt
+++ crypto/src/appleMain/kotlin/at/asitplus/awesn1/crypto/pki/X509TbsCertificate.apple.kt
@@ -1,18 +1,22 @@
+@file:OptIn(ExperimentalObjCName::class)
 // SPDX-FileCopyrightText: Copyright (c) A-SIT Plus GmbH
 // SPDX-License-Identifier: Apache-2.0
 
 package at.asitplus.awesn1.crypto.pki
 
 import at.asitplus.awesn1.Asn1BitString
+import kotlin.experimental.ExperimentalObjCName
 
 /**
  * @see X509TbsCertificate.issuerUniqueID
  */
 @Throws(IllegalArgumentException::class)
-fun X509TbsCertificate.issuerUniqueID(): Asn1BitString? = issuerUniqueID
+@ObjCName("issuerUniqueID")
+fun __apple_workaround_X509TbsCertificate_issuerUniqueID(it: X509TbsCertificate) = it.issuerUniqueID
 
 /**
  * @see X509TbsCertificate.subjectUniqueID
  */
 @Throws(IllegalArgumentException::class)
-fun X509TbsCertificate.subjectUniqueID(): Asn1BitString? = subjectUniqueID
+@ObjCName("subjectUniqueID")
+fun __apple_workaround_X509TbsCertificate_subjectUniqueID(it: X509TbsCertificate) = it.subjectUniqueID
